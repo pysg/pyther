76d238d616af00d25a80d394697e910d497fe085
pysg <andres.python@gmail.com>
2017-01-16 06:10:45 -0300

    solid pure

--- properties_ecuations.py
+++ properties_ecuations.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import pyther as pt
 
@@ -15,68 +16,41 @@ component = "ISOBUTANE"
 
 properties_data = pt.Data_parse()
 
-
-
 def read_dppr(dppr_file):
         #self.dppr_data = pd.read_excel(dppr_file).head().set_index('Name').ix[:, 1:12]
 
         #dppr_data = pd.read_excel(dppr_file).set_index("Name").ix[:, 1:5]
-        dppr_data = pd.read_excel(dppr_file).ix[:, 0:6]
+        dppr_data = pd.read_excel(dppr_file).ix[:, 0:8]
         
         # component_names = dppr_data.index.get_values()
         return dppr_data
-    
-
 
 data = read_dppr(dppr_file)
+
 print(data)
 
-data_grupo = data.ix[0:12, 0:6]
+g = 3
+components_labels = [x for x in range(0, 13*g, 13)]
+data_name = data.ix[components_labels, 0].get_values()
 
-print(data.ix[0,0], data.ix[13,0], data.ix[26,0])
+def rho_liq_cal(component):
+	
+	rho_liquido_constans = [x+1 for x in range(0, 13*g, 13)]
+	datos_rho_liquido = data.ix[rho_liquido_constans, 1:8].get_values()
 
+	print(datos_rho_liquido)
+	datos_rho_liquido = pd.DataFrame(data=datos_rho_liquido,index=data_name,
+						 columns=["A", "B", "C", "D", "E", "Min", "Max"])
 
-for i in range(0, 13*10, 13):
-	print(data.ix[i,0])
-
-
-
-
-def selec_component(dppr_file, component):
-        name = str(component)
-        properties = read_dppr(dppr_file).loc[name]
-        label = {name : properties}
-
-        #print(self.properties.index)
-        #print ('name = {0}'.format(self.name))
-        #print ('acentric_factor = {0}'.format(self.properties['Omega']))
-        #print ('critical_Temperature = {0}'.format(self.properties['Tc']))
-        #print ('critical_Pressure = {0}'.format(self.properties['Pc']))
-        #print ('compressibility_factor_Z = {0}'.format(self.properties['Zc']))
-        #print(self.label.keys())
-        #print(self.label.values())
-
-        return name, properties
-
-#selec_component(dppr_file, component)
+	rho_liquido_constans_component = datos_rho_liquido.loc[component]
 
 
+	return rho_liquido_constans_component
 
+# Liquid_density [kmol/m^3]
+# A / B ^ (1 + (1 - T / C) ^ D)
+A, B, C, D, E, Min, Max = rho_liq_cal("ETHANE")
 
-
-#component_eos_list = np.zeros( (len(components),4) )
-#dinputs = np.zeros( (len(components),4) )
-
-
-#for index, component in enumerate(components):
-
-#    properties_component = properties_data.selec_component(dppr_file, component)
-#    pt.print_properties_component(component, properties_component)
-#    dinputs[index] = np.array([properties_component[1]['Tc'], properties_component[1]['Pc'],
-#                        properties_component[1]['Omega'], properties_component[1]['Vc']])
-
-
-
-#components_table = pd.DataFrame(component_eos_list, index=components, columns=['ac', 'b', 'rm', 'del1'])
-
-#print(dinputs)
+Temp_vector = np.array([Temp_vector for Temp_vector in np.arange(Min, Max)])
+rho_liquido = A / B ** (1 + (1 - Temp_vector / C) ** D)
+print(rho_liquido)
