55b165d6c16da48684f352705d0b8a82193c3f98
pysg <andres.python@gmail.com>
2017-01-25 21:15:21 -0300

    solid pure

--- properties_ecuations.py
+++ properties_ecuations.py
@@ -1,11 +1,6 @@
 import numpy as np
 import pandas as pd
 #import pyther as pt
-#----------------------------------------------------------------------------------------------------------------
-
- 	
-
-#----------------------------------------------------------------------------------------------------------------
 
 # constan to size of data base of components
 size_data = 30
@@ -34,52 +29,8 @@ class Thermodynamic_correlations(object):
 		return self.data_name
 
 	# properties thermodynamics
-	def fun_ther(self, property_thermodynamics):
+	def select_property(self, property_thermodynamics):
 
-		if property_thermodynamics == "Solid_Density":
-			Solid_Density = "Solid Density", "[kmol/m^3]", "A+B*T+C*T^2+D*T^3+E*T^4", 0
-			return Solid_Density
-		elif property_thermodynamics == "Liquid_Density":
-			Liquid_Density = "Liquid Density", "[kmol/m^3]", "A/B^(1+(1-T/C)^D)", 1
-			return Liquid_Density 
-		elif property_thermodynamics ==  "Vapour_Pressure":
-			Vapour_Pressure = "Vapour Pressure", "[Pa]", "exp(A+B/T+C*ln(T)+D*T^E)", 2
-			return Vapour_Pressure
-		elif property_thermodynamics == "Heat_of_Vaporization":
-			Heat_of_Vaporization = "Heat of Vaporization", "[J/kmol]", "A*(1-Tr)^(B+C*Tr+D*Tr^2)", 3
-			return Heat_of_Vaporization 
-		elif property_thermodynamics == "solid_Heat_Capacity":
-			Solid_Heat_Capacity = "Solid Heat Capacity", "[J/(kmol*K)]", "A+B*T+C*T^2+D*T^3+E*T^4", 4
-			return Solid_Heat_Capacity
-		elif property_thermodynamics == "Liquid_Heat_Capacity":
-			Liquid_Heat_Capacity = "Liquid Heat Capacity", "[J/(kmol*K)]", "A^2/(1-Tr)+B-2*A*C*(1-Tr)-A*D*(1-Tr)^2-C^2*(1-Tr)^3/3-C*D*(1-Tr)^4/2-D^2*(1-Tr)^5/5", 5
-			return Liquid_Heat_Capacity
-		elif property_thermodynamics == "ideal_Gas_Heat_Capacity":
-			Ideal_Gas_Heat_Capacity = "Ideal Gas Heat Capacity" "[J/(kmol*K)]", "A+B*(C/T/sinh(C/T))^2+D*(E/T/cosh(E/T))^2", 6
-			return Ideal_Gas_Heat_Capacity
-		elif property_thermodynamics == "Second_Virial_Coefficient": 
-			Second_Virial_Coefficient = "Second	Virial	Coefficient", "[m^3/kmol]", "A+B/T+C/T^3+D/T^8+E/T^9", 7
-			return Second_Virial_Coefficient
-		elif property_thermodynamics == "Liquid_Viscosity": 
-			Liquid_Viscosity = "Liquid	Viscosity", "[kg/(m*s)]", "exp(A+B/T+C*ln(T)+D*T^E)", 8
-			return Liquid_Viscosity
-		elif property_thermodynamics == "Vapour_Viscosity": 
-			Vapour_Viscosity = "Vapour	Viscosity", "[kg/(m*s)]", "A*T^B/(1+C/T+D/T^2)", 9
-			return Vapour_Viscosity
-		elif property_thermodynamics == "Liquid_Thermal_Conductivity": 
-			Liquid_Thermal_Conductivity = "Liquid Thermal Conductivity", "[J/(m*s*K)]", "A+B*T+C*T^2+D*T^3+E*T^4", 10
-			return liquid_Thermal_Conductivity
-		elif property_thermodynamics == "Vapour_Thermal_Conductivity": 
-			Vapour_Thermal_Conductivity = "Vapour Thermal Conductivity", "[J/(m*s*K)]", "A*T^B/(1+C/T+D/T^2)", 11
-			return Vapour_Thermal_Conductivity
-		elif property_thermodynamics == "surface_Tension": 
-			Surface_Tension = "Surface Tension", "[kg/s^2]", "A*(1-Tr)^(B+C*Tr+D*Tr^2)", 12	
-			return Surface_Tension
-
-
-
-
-	def property_cal(self, component, property_thermodynamics, temperature = None):
 		"""
 		properties thermodynamics
 
@@ -98,22 +49,69 @@ class Thermodynamic_correlations(object):
 		Surface_Tension = "Surface Tension", "[kg/s^2]", "A*(1-Tr)^(B+C*Tr+D*Tr^2)", 12	
 		"""
 
-		thermo = self.fun_ther(property_thermodynamics)
 
-		#select_constans = [x + property_thermodynamics[3] for x in range(0, 13*size_data, 13)]
-		select_constans = [x + thermo[3] for x in range(0, 13*size_data, 13)]
+		if property_thermodynamics == "Solid_Density":
+			Solid_Density = "Solid Density", "[kmol/m^3]", "A+B*T+C*T^2+D*T^3+E*T^4", 0
+			return Solid_Density
+		elif property_thermodynamics == "Liquid_Density":
+			Liquid_Density = "Liquid Density", "[kmol/m^3]", "A/B^(1+(1-T/C)^D)", 1
+			return Liquid_Density 
+		elif property_thermodynamics == "Vapour_Pressure":
+			Vapour_Pressure = "Vapour Pressure", "[Pa]", "exp(A+B/T+C*ln(T)+D*T^E)", 2
+			return Vapour_Pressure
+		elif property_thermodynamics == "Heat_of_Vaporization":
+			Heat_of_Vaporization = "Heat of Vaporization", "[J/kmol]", "A*(1-Tr)^(B+C*Tr+D*Tr^2)", 3
+			return Heat_of_Vaporization 
+		elif property_thermodynamics == "solid_Heat_Capacity":
+			Solid_Heat_Capacity = "Solid Heat Capacity", "[J/(kmol*K)]", "A+B*T+C*T^2+D*T^3+E*T^4", 4
+			return Solid_Heat_Capacity
+		elif property_thermodynamics == "Liquid_Heat_Capacity":
+			
+			Liquid_Heat_Capacity = "Liquid Heat Capacity", "[J/(kmol*K)]", "A^2/(1-Tr)+B-2*A*C*(1-Tr)-A*D*(1-Tr)^2-C^2*(1-Tr)^3/3-C*D*(1-Tr)^4/2-D^2*(1-Tr)^5/5", 5
+			return Liquid_Heat_Capacity
 		
+		elif property_thermodynamics == "Ideal_Gas_Heat_Capacity":
+		
+			Ideal_Gas_Heat_Capacity = "Ideal Gas Heat Capacity", "[J/(kmol*K)]", "A+B*(C/T/sinh(C/T))^2+D*(E/T/cosh(E/T))^2", 6
+			return Ideal_Gas_Heat_Capacity
+		
+		elif property_thermodynamics == "Second_Virial_Coefficient": 
+		
+			Second_Virial_Coefficient = "Second	Virial	Coefficient", "[m^3/kmol]", "A+B/T+C/T^3+D/T^8+E/T^9", 7
+			return Second_Virial_Coefficient
+		
+		elif property_thermodynamics == "Liquid_Viscosity": 
+			Liquid_Viscosity = "Liquid	Viscosity", "[kg/(m*s)]", "exp(A+B/T+C*ln(T)+D*T^E)", 8
+			return Liquid_Viscosity
+		elif property_thermodynamics == "Vapour_Viscosity": 
+			Vapour_Viscosity = "Vapour	Viscosity", "[kg/(m*s)]", "A*T^B/(1+C/T+D/T^2)", 9
+			return Vapour_Viscosity
+		elif property_thermodynamics == "Liquid_Thermal_Conductivity": 
+			Liquid_Thermal_Conductivity = "Liquid Thermal Conductivity", "[J/(m*s*K)]", "A+B*T+C*T^2+D*T^3+E*T^4", 10
+			return liquid_Thermal_Conductivity
+		elif property_thermodynamics == "Vapour_Thermal_Conductivity": 
+			Vapour_Thermal_Conductivity = "Vapour Thermal Conductivity", "[J/(m*s*K)]", "A*T^B/(1+C/T+D/T^2)", 11
+			return Vapour_Thermal_Conductivity
+		elif property_thermodynamics == "surface_Tension": 
+			Surface_Tension = "Surface Tension", "[kg/s^2]", "A*(1-Tr)^(B+C*Tr+D*Tr^2)", 12	
+			return Surface_Tension
+
+	def property_cal(self, component, property_thermodynamics, temperature = None):
+
+		property_label = self.select_property(property_thermodynamics)
+
+		select_constans = [x + property_label[3] for x in range(0, 13*size_data, 13)]	
 		values_constans = self.read_dppr().ix[select_constans, 1:8].get_values()
-		table_constans = pd.DataFrame(data=values_constans,index=self.data_name_cal(),
+		self.table_constans = pd.DataFrame(data=values_constans,index=self.data_name_cal(),
 							 columns=["A", "B", "C", "D", "E", "T Min [K]", "T Max [K]"])
 
-		print(table_constans)
-		component_constans = table_constans.loc[component]
-		print(component_constans)
+		#print(table_constans)
+		self.component_constans = self.table_constans.loc[component]
+		#print(component_constans)
 
 		#Min, Max = np.zeros(2), np.array(2)
 
-		A, B, C, D, E, Min, Max = component_constans.get_values()
+		A, B, C, D, E, Min, Max = self.component_constans.get_values()
 
 		#print("sss = ",Min, Max)
 
@@ -175,6 +173,8 @@ class Thermodynamic_correlations(object):
 			surface_Tension = A*(1-Tr)**(B+C*Tr+D*Tr*2)
 			return surface_Tension
 
+	
+
 
 def main():
 
@@ -200,11 +200,16 @@ def main():
 	temp = [180.4, 181.4, 185.3, 210, 85]
 	#temp = 180.4
 
+	#property_thermodynamics = thermodynamic_correlations.property_cal(component, "Vapour_Pressure", temp)
 	property_thermodynamics = thermodynamic_correlations.property_cal(component, "Vapour_Pressure", temp)
+	#property_thermodynamics = thermodynamic_correlations.property_cal(component, "Ideal_Gas_Heat_Capacity", temp)
 	#property_thermodynamics = property_cal(components, Vapour_Pressure, temp)
 	#property_thermodynamics = property_cal(component, Vapour_Pressure, [180.4, 181.4, 185.3, 210, 85])
 	#property_thermodynamics = thermodynamic_correlations.property_cal(component, Vapour_Pressure)
 	print(property_thermodynamics)
+	print(thermodynamic_correlations.table_constans)
+
+	"[ 33382.25795338  33386.53987038  33404.81508022  33591.00924113 33298.00165497]"
 
 	print('-' * 79)
 
