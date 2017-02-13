a1e5c9cadc3cac6393a6a5dbb0138be907677b93
pysg <andres.python@gmail.com>
2017-02-13 11:30:41 -0300

    pvt envelope

--- campana.py
+++ campana.py
@@ -85,7 +85,7 @@ if FMAX > FMAXOLD and DMAX > DMAXOLD or NITER == 10:
 		if XOLD[0] != 0:
 			pass
 
-
+#---------------------------------------------------------------------------
 
 #    NSOLD=NS
 
@@ -101,6 +101,8 @@ if VV > 1.1 * VC:
 	NS = 1
 else:
 	NS = 3
+
+#----------------------------------------------------------------------------
 
 #    if(NS.ne.NSOLD)then
 #        dSdSold=dXdS(NS)
@@ -123,13 +125,7 @@ if NS != NSOLD:
 NITER = min(niter,10)
 delXS = delXS * 5 / NITER / dXdS[NSOLD]
 
-
-
-
-
-
-
-
+#------------------------------------------------------------------------
 
 #IF(delXS.LT.0)then
 #        if(NS.EQ.1)
@@ -263,7 +259,7 @@ if (Pv > 1e-20 and (T > 0.25 * TCmod or T > 250) and T < TCmod):
 
 
 
-def XTVTERMO_cal(INDIC,T,V,P,rn, FUGLOG,DLFUGT,DLFUGV,DLFUGX):
+def XTVTERMO_cal(INDIC, T, V, P, rn):
 
 	NG = NGR
 	NC = 2
@@ -272,7 +268,7 @@ def XTVTERMO_cal(INDIC,T,V,P,rn, FUGLOG,DLFUGT,DLFUGV,DLFUGX):
 	NDER = 1
 
 	if INDIC > 2:
-		NDER =2
+		NDER = 2
 
 	if INDIC > 2 or INDIC == 4:
 		NTEMP = 1
@@ -317,101 +313,116 @@ def XTVTERMO_cal(INDIC,T,V,P,rn, FUGLOG,DLFUGT,DLFUGV,DLFUGX):
 	DLFUGX[K, I] = DLFUGX[I, K]
 	DLFUGX[I, I] = DLFUGX[I, I] + 1.0 / rn[I]
 
+	return FUGLOG, DLFUGT, DLFUGV, DLFUGX
+
+
+def SUBROUTINE ArVnder(nc,NDER,NTD,rn,V,T,Ar,ArV,ArTV,ArV2,Arn,ArVn,ArTn,Arn2):
+    
+    IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+	dimension rn(nc),Arn(nc),ArVn(nc),ArTn(nc),Arn2(nc,nc)
+	COMMON /MODEL/ NMODEL
+	
+	IF(NMODEL.LE.2)THEN	
+		#!  SRK or PR
+		CALL HelmSRKPR(nc,NDER,NTD,rn,V,T,Ar,ArV,ArTV,ArV2,Arn,ArVn,ArTn,Arn2)	
+	ELSE IF (NMODEL.EQ.3) THEN
+		CALL HelmRKPR(nc,NDER,NTD,rn,V,T,Ar,ArV,ArTV,ArV2,Arn,ArVn,ArTn,Arn2)
+ 	
+    END
 
 
 
 
 
+def SUBROUTINE HelmRKPR():
+	#---------------------------------------
+
+	#HelmRKPR
+
+	#	SUBROUTINE HelmRKPR(nco,NDE,NTD,rn,V,T,Ar,ArV,ArTV,ArV2,Arn,ArVn,ArTn,Arn2)
+	#    IMPLICIT DOUBLE PRECISION (A-H,O-Z)
+	#    PARAMETER (RGAS=0.08314472d0) 
+	#	dimension rn(nco),Arn(nco),ArVn(nco),ArTn(nco),Arn2(nco,nco)
+	#	dimension dBi(nco),dBij(nco,nco),dD1i(nco),dD1ij(nco,nco)
+	#	dimension dDi(nco),dDij(nco,nco),dDiT(nco)
+		dimension aij(nco,nco),daijdT(nco,nco),daijdT2(nco,nco)
+		COMMON /rule/ncomb
+		nc=nco
+		TOTN = sum(rn)
+		call DELTAnder(nc,rn,D1,dD1i,dD1ij)
+		D2=(1-D1)/(1+D1)
+
+	#!  Comparison to test and debug cubic mixing rules
+	#!  rn=[0.65,0.35]
+	#!  T=460.0d0
+	#!  	call Bnder(nc,rn,Bmix,dBi,dBij)
+	#!  	call Bcubicnder(nc,rn,Bmix,dBi,dBij)
+	#!  	call DandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
+	#!  	call DCubicandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
+
+		if(ncomb.lt.2)then
+			call Bnder(nc,rn,Bmix,dBi,dBij)
+			call DandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
+		else
+	#!  	call Bcubicnder(nc,rn,Bmix,dBi,dBij)
+	#!  	call DCubicandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
+		end if
+	#!  The f's and g's used here are for Ar, not F (reduced Ar)					***********
+	#!  This requires to multiply by R all g, f and its derivatives as defined by Mollerup ****
+		f=log((V+D1*Bmix)/(V+D2*Bmix))/Bmix/(D1-D2)
+		g=RGAS*log(1-Bmix/V)
+		fv=-1/((V+D1*Bmix)*(V+D2*Bmix))
+		fB=-(f+V*fv)/Bmix
+		gv=RGAS*Bmix/(V*(V-Bmix))
+		fv2=(-1/(V+D1*Bmix)**2+1/(V+D2*Bmix)**2)/Bmix/(D1-D2)
+		gv2=RGAS*(1/V**2-1/(V-Bmix)**2)
+	#!  DERIVATIVES OF f WITH RESPECT TO DELTA1
+		auxD2=(1+2/(1+D1)**2)
+		fD1=(1/(V+D1*Bmix)+2/(V+D2*Bmix)/(1+D1)**2)-f*auxD2
+		fD1=fD1/(D1-D2)
+		fBD1=-(fB*auxD2+D1/(V+D1*Bmix)**2+2*D2/(V+D2*Bmix)**2/(1+D1)**2)
+		fBD1=fBD1/(D1-D2)
+		fVD1=-(fV*auxD2+1/(V+D1*Bmix)**2+2/(V+D2*Bmix)**2/(1+D1)**2)/(D1-D2)
+		fD1D1=4*(f-1/(V+D2*Bmix))/(1+D1)**3+Bmix*(-1/(V+D1*Bmix)**2+ &
+				4/(V+D2*Bmix)**2/(1+D1)**4)-2*fD1*(1+2/(1+D1)**2)
+		fD1D1=fD1D1/(D1-D2)
+	#!  Reduced Helmholtz Energy and derivatives
+		Ar=-TOTN*g*T-D*f
+		ArV=-TOTN*gv*T-D*fv
+		ArV2=-TOTN*gv2*T-D*fv2
+
+		AUX=RGAS*T/(V-Bmix)
+		FFB=TOTN*AUX-D*fB
+		FFBV=-TOTN*AUX/(V-Bmix)+D*(2*fv+V*fv2)/Bmix
+		FFBB=TOTN*AUX/(V-Bmix)-D*(2*f+4*V*fv+V**2*fv2)/Bmix**2
+		do i=1,nc
+		Arn(i)=-g*T+FFB*dBi(i)-f*dDi(i)-D*fD1*dD1i(i)
+		ArVn(i)=-gv*T+FFBV*dBi(i)-fv*dDi(i)-D*fVD1*dD1i(i)
+		IF (NDE.EQ.2) THEN
+		do j=1,i
+		Arn2(i,j)=AUX*(dBi(i)+dBi(j))-fB*(dBi(i)*dDi(j)+dBi(j)*dDi(i))  &
+	     		+FFB*dBij(i,j)+FFBB*dBi(i)*dBi(j)-f*dDij(i,j)      
+	      Arn2(i,j)=Arn2(i,j)-D*fBD1*(dBi(i)*dD1i(j)+dBi(j)*dD1i(i))    &
+	     		-fD1*(dDi(i)*dD1i(j)+dDi(j)*dD1i(i))        &
+	     		-D*fD1*dD1ij(i,j)-D*fD1D1*dD1i(i)*dD1i(j)
+		Arn2(j,i)=Arn2(i,j)
+		end do
+		END IF
+		end do
+	#!  TEMPERATURE DERIVATIVES
+		IF (NTD.EQ.1) THEN
+		ArT=-TOTN*g-dDdT*f
+		ArTV=-TOTN*gv-dDdT*fV
+		ArTT=-dDdT2*f
+		do i=1,nc
+		ArTn(i)=-g+(TOTN*AUX/T-dDdT*fB)*dBi(i)-f*dDiT(i)-dDdT*fD1*dD1i(i)
+		end do
+		END IF
+		end
+	#-----------------------------------------------------------------
 
 
 
-
-
-#---------------------------------------
-
-#HelmRKPR
-
-#	SUBROUTINE HelmRKPR(nco,NDE,NTD,rn,V,T,Ar,ArV,ArTV,ArV2,Arn,ArVn,ArTn,Arn2)
-#    IMPLICIT DOUBLE PRECISION (A-H,O-Z)
-#    PARAMETER (RGAS=0.08314472d0) 
-#	dimension rn(nco),Arn(nco),ArVn(nco),ArTn(nco),Arn2(nco,nco)
-#	dimension dBi(nco),dBij(nco,nco),dD1i(nco),dD1ij(nco,nco)
-#	dimension dDi(nco),dDij(nco,nco),dDiT(nco)
-	dimension aij(nco,nco),daijdT(nco,nco),daijdT2(nco,nco)
-	COMMON /rule/ncomb
-	nc=nco
-	TOTN = sum(rn)
-	call DELTAnder(nc,rn,D1,dD1i,dD1ij)
-	D2=(1-D1)/(1+D1)
-
-#!  Comparison to test and debug cubic mixing rules
-#!  rn=[0.65,0.35]
-#!  T=460.0d0
-#!  	call Bnder(nc,rn,Bmix,dBi,dBij)
-#!  	call Bcubicnder(nc,rn,Bmix,dBi,dBij)
-#!  	call DandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
-#!  	call DCubicandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
-
-	if(ncomb.lt.2)then
-		call Bnder(nc,rn,Bmix,dBi,dBij)
-		call DandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
-	else
-#!  	call Bcubicnder(nc,rn,Bmix,dBi,dBij)
-#!  	call DCubicandTnder(NTD,nc,T,rn,D,dDi,dDiT,dDij,dDdT,dDdT2)
-	end if
-#!  The f's and g's used here are for Ar, not F (reduced Ar)					***********
-#!  This requires to multiply by R all g, f and its derivatives as defined by Mollerup ****
-	f=log((V+D1*Bmix)/(V+D2*Bmix))/Bmix/(D1-D2)
-	g=RGAS*log(1-Bmix/V)
-	fv=-1/((V+D1*Bmix)*(V+D2*Bmix))
-	fB=-(f+V*fv)/Bmix
-	gv=RGAS*Bmix/(V*(V-Bmix))
-	fv2=(-1/(V+D1*Bmix)**2+1/(V+D2*Bmix)**2)/Bmix/(D1-D2)
-	gv2=RGAS*(1/V**2-1/(V-Bmix)**2)
-#!  DERIVATIVES OF f WITH RESPECT TO DELTA1
-	auxD2=(1+2/(1+D1)**2)
-	fD1=(1/(V+D1*Bmix)+2/(V+D2*Bmix)/(1+D1)**2)-f*auxD2
-	fD1=fD1/(D1-D2)
-	fBD1=-(fB*auxD2+D1/(V+D1*Bmix)**2+2*D2/(V+D2*Bmix)**2/(1+D1)**2)
-	fBD1=fBD1/(D1-D2)
-	fVD1=-(fV*auxD2+1/(V+D1*Bmix)**2+2/(V+D2*Bmix)**2/(1+D1)**2)/(D1-D2)
-	fD1D1=4*(f-1/(V+D2*Bmix))/(1+D1)**3+Bmix*(-1/(V+D1*Bmix)**2+ &
-			4/(V+D2*Bmix)**2/(1+D1)**4)-2*fD1*(1+2/(1+D1)**2)
-	fD1D1=fD1D1/(D1-D2)
-#!  Reduced Helmholtz Energy and derivatives
-	Ar=-TOTN*g*T-D*f
-	ArV=-TOTN*gv*T-D*fv
-	ArV2=-TOTN*gv2*T-D*fv2
-
-	AUX=RGAS*T/(V-Bmix)
-	FFB=TOTN*AUX-D*fB
-	FFBV=-TOTN*AUX/(V-Bmix)+D*(2*fv+V*fv2)/Bmix
-	FFBB=TOTN*AUX/(V-Bmix)-D*(2*f+4*V*fv+V**2*fv2)/Bmix**2
-	do i=1,nc
-	Arn(i)=-g*T+FFB*dBi(i)-f*dDi(i)-D*fD1*dD1i(i)
-	ArVn(i)=-gv*T+FFBV*dBi(i)-fv*dDi(i)-D*fVD1*dD1i(i)
-	IF (NDE.EQ.2) THEN
-	do j=1,i
-	Arn2(i,j)=AUX*(dBi(i)+dBi(j))-fB*(dBi(i)*dDi(j)+dBi(j)*dDi(i))  &
-     		+FFB*dBij(i,j)+FFBB*dBi(i)*dBi(j)-f*dDij(i,j)      
-      Arn2(i,j)=Arn2(i,j)-D*fBD1*(dBi(i)*dD1i(j)+dBi(j)*dD1i(i))    &
-     		-fD1*(dDi(i)*dD1i(j)+dDi(j)*dD1i(i))        &
-     		-D*fD1*dD1ij(i,j)-D*fD1D1*dD1i(i)*dD1i(j)
-	Arn2(j,i)=Arn2(i,j)
-	end do
-	END IF
-	end do
-#!  TEMPERATURE DERIVATIVES
-	IF (NTD.EQ.1) THEN
-	ArT=-TOTN*g-dDdT*f
-	ArTV=-TOTN*gv-dDdT*fV
-	ArTT=-dDdT2*f
-	do i=1,nc
-	ArTn(i)=-g+(TOTN*AUX/T-dDdT*fB)*dBi(i)-f*dDiT(i)-dDdT*fD1*dD1i(i)
-	end do
-	END IF
-	end
-#-----------------------------------------------------------------
 
 def HelmRKPR(nco,NDE,NTD,rn,V,T,Ar,ArV,ArTV,ArV2,Arn,ArVn,ArTn,Arn2):
     # IMPLICIT DOUBLE PRECISION (A-H,O-Z)
