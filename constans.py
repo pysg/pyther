6862b28e7e1f8c24852db703e1ff9140f5172783
pysg <andres.python@gmail.com>
2017-04-22 00:01:33 -0300

    envelop

--- constans.py
+++ constans.py
@@ -11,3 +11,6 @@ A1, B1, C1 = -2.4407, 7.4513, 12.504
 # Definir el significado fisicoquímico
 
 D = np.array([0.428363, 18.496215, 0.338426, 0.660, 789.723105, 2.512392])
+
+
+# definir la correlación de los valores para los paramteros A0, B0, C0 y A1, B1, C1 y D
