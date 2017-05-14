eda41774451e74f47b94a0707db02c20539d22e8
pysg <andres.python@gmail.com>
2017-05-13 23:13:16 -0300

    eos parameters

--- cubic_parameters_1.py
+++ cubic_parameters_1.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy.optimize import fsolve
 from .eos_selecction import eos, convert_argument
 
-#from constants import RGAS, A0, B0, C0, A1, B1, C1, D
+from constants import RGAS, A0, B0, C0, A1, B1, C1, D
 
 '''
 This module calculate parameters necessary to use the equations of state:
@@ -10,16 +10,16 @@ SRK, PR and RKPR
 '''
 
 # Constante universal de los gases R
-RGAS = 0.08314472
+#RGAS = 0.08314472
 
 # Definir el significado fisicoquímico
-A0, B0, C0 = 0.0017, 1.9681, -2.7238
+#A0, B0, C0 = 0.0017, 1.9681, -2.7238
 
 # Definir el significado fisicoquímico
-A1, B1, C1 = -2.4407, 7.4513, 12.504
+#A1, B1, C1 = -2.4407, 7.4513, 12.504
 
 # Definir el significado fisicoquímico
-D = np.array([0.428363, 18.496215, 0.338426, 0.660, 789.723105, 2.512392])
+#D = np.array([0.428363, 18.496215, 0.338426, 0.660, 789.723105, 2.512392])
 
 
 def getdel1(Zcin, del_1_init):
