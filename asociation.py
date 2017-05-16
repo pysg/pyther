ddd538c05f70623f863b24fa743f5e998c85b954
pysg <andres.python@gmail.com>
2017-05-15 23:52:09 -0300

    eos parameters

---
+++ asociation.py
@@ -0,0 +1,18 @@
+
+
+"auto_asociation"
+
+"carboxilo" : "-C=OOH"
+"hidroxilo" : "-OH"
+"ester" : "-C=OO-"
+"cetona" : "-C=O"
+
+energy_asociation != 0
+volume_asociation != 0
+
+force_asociation != 0
+
+
+
+
+"cross_asociation"
