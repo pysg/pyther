d9b8cf0ac4eb2434940a24c7dedb68ac51ea95e6
pysg <andres.python@gmail.com>
2017-05-24 09:21:14 -0300

    eos parameters

--- cubic_parameters_1.py
+++ cubic_parameters_1.py
@@ -11,18 +11,6 @@ This module calculate parameters necessary to use the equations of state:
 SRK, PR and RKPR
 '''
 
-# Constante universal de los gases R
-# RGAS = 0.08314472
-
-# Definir el significado fisicoquímico
-# A0, B0, C0 = 0.0017, 1.9681, -2.7238
-
-# Definir el significado fisicoquímico
-# A1, B1, C1 = -2.4407, 7.4513, 12.504
-
-# Definir el significado fisicoquímico
-# D = np.array([0.428363, 18.496215, 0.338426, 0.660, 789.723105, 2.512392])
-
 
 class Parameter_eos(object):
     '''
