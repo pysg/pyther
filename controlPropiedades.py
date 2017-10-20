62a1a99a38c510c552cbc2131608cb9d06ca9497
andrespysg <andres.python@gmail.com>
2017-10-20 04:34:12 -0300

    controlPropiedades

--- controlPropiedades.py
+++ controlPropiedades.py
@@ -1,99 +1,138 @@
 import numpy as np
 
 
-if property_thermodynamics == "Solid_Density":
-	solid_Density = A + B * Temp_vector + C * Temp_vector ** 2 + D * Temp_vector ** 3 + E * Temp_vector ** 4
+
+def solid_Density(T):
+	solid_Density = A + B * T + C * T ** 2 + D * T ** 3 + E * T ** 4
 	return solid_Density
-elif property_thermodynamics == "Liquid_Density":
-	liquid_Density = A / B ** (1 + (1 - Temp_vector / C) ** D)
+
+def liquid_Density(T):
+	liquid_Density = A / B ** (1 + (1 - T / C) ** D)
 	return liquid_Density
-elif property_thermodynamics == "Vapour_Pressure":
+
+def vapour_Pressure(T):
 	# used 1 Pa = 1e-5 Bar
-
-	if temperature == None:
-		# without temperature especific
-
-		if len(self.components) == 1:
-			# one component without one temperature especific
-
-			log_tem = np.log(np.float64(Temp_vector))
-			log_vapour_Pressure = A + B/Temp_vector + C * log_tem + D*Temp_vector **E
-			vapour_Pressure = np.exp(np.float64(log_vapour_Pressure)) * 1e-5
-
-		else:
-			# many components 
-			log_tem = np.array([np.log(Temp_vector) for Temp_vector in Temp_vector])
-			vapour = np.array(A + B/Temp_vector + C * log_tem + D*Temp_vector **E)
-			vapour_Pressure = np.array([np.exp(vapour) for vapour in vapour]) * 1e-5
-			#print("vapour_Pressure = ", vapour_Pressure, np.shape(vapour_Pressure))
-
-	else:
-
-		if len(self.components) == 1:
-			# one component with one temperature especific 
-
-			log_tem = np.array([np.log(Temp_vector) for Temp_vector in Temp_vector])
-			log_vapour_Pressure = A + B/Temp_vector + C * log_tem + D*Temp_vector **E
-			#print("log_vapour-pressure = ",log_vapour_Pressure)
-			vapour_Pressure = np.array([np.exp(vapour) for vapour in log_vapour_Pressure]) * 1e-5
-		else:
-			# many components
-
-			if len(temperature) == 1:
-				# many components with one temperature especific
-
-				log_tem = np.array([np.log(Temp_vector) for Temp_vector in Temp_vector])
-				vapour = np.array(A + B/Temp_vector.T + C * log_tem.T + D*Temp_vector.T **E)
-				# used 1 Pa = 1e-5 Bar
-				vapour_Pressure = np.array([np.exp(vapour) for vapour in vapour]) * 1e-5
-				print("vapour_Pressure = ", vapour_Pressure, np.shape(vapour_Pressure))
-
-			else:
-				# many components with many temperatures especific
-
-				log_tem = [np.log(Temp_vector) for Temp_vector in Temp_vector]
-				log_vapour_Pressure = A + B/Temp_vector + C * log_tem + D*Temp_vector **E
-				#print("log_vapour-pressure = ",log_vapour_Pressure)
-				vapour_Pressure = np.array([np.exp(vapour) for vapour in log_vapour_Pressure]) * 1e-5
-
-			print(np.size(vapour_Pressure))
-
+	vapour_Pressure = np.exp(A + B / T + C * log_tem + D * T ** E) * 1e-5
 	return vapour_Pressure
 
-
-
-
-
-
-
-
-elif property_thermodynamics == "Heat_of_Vaporization":
+def heat_of_Vaporization(T, Tc):
+	Tr = T / Tc
 	heat_of_Vaporization = A * (1 - Tr) ** (B + C * Tr + D * Tr ** 2)
-	return heat_of_Vaporization
-elif property_thermodynamics == "Solid_Heat_Capacity":
-	solid_Heat_Capacity = A + B * Temp_vector + C * Temp_vector ** 2 + D * Temp_vector ** 3 + E * Temp_vector ** 4
+	pass
+
+def solid_Heat_Capacity(T):
+	solid_Heat_Capacity = A + B * T + C * T ** 2 + D * T ** 3 + E * T ** 4
 	return solid_Heat_Capacity
-elif property_thermodynamics == "Liquid_Heat_Capacity":
-	liquid_Heat_Capacity = A ** 2 / (1 - Tr) + B - 2 * A * C * (1 - Tr) - A * D * (1 - Tr) ** 2 -C ** 2 * (1 - Tr) ** 3 / 3 - C * D * (1-Tr)**4/2-D**2*(1-Tr)**5/5
-	return(liquid_Heat_Capacity)
-elif property_thermodynamics == "Ideal_Gas_Heat_Capacity":
-	ideal_Gas_Heat_Capacity = A + B * (C / Temp_vector/np.sinh(C/Temp_vector))**2+D*(E/Temp_vector/np.cosh(E/Temp_vector))**2
+
+def liquid_Heat_Capacity(T, Tc):
+	Tr = T / Tc
+
+	variable1 = A ** 2 / (1 - Tr) + B - 2 * A * C * (1 - Tr)
+	variable2 = - A * D * (1 - Tr) ** 2 -C ** 2 * (1 - Tr) ** 3 / 3
+	variable3 = - C * D * (1-Tr) ** 4 / 2 - D ** 2 * (1 - Tr) ** 5 / 5
+
+	liquid_Heat_Capacity = variable1 + variable2 + variable3
+	return liquid_Heat_Capacity
+
+def ideal_Gas_Heat_Capacity(T):
+	ideal_Gas_Heat_Capacity = A + B * (C / T/np.sinh(C / T)) ** 2 + D * (E/T/np.cosh(E / T)) ** 2
 	return ideal_Gas_Heat_Capacity
-elif property_thermodynamics == "Second_Virial_Coefficient":
-	second_Virial_Coefficient = A+B/Temp_vector+C/Temp_vector**3+D/Temp_vector**8+E/Temp_vector**9
+
+def second_Virial_Coefficient(T):
+	second_Virial_Coefficient = A + B / T + C / T ** 3 + D / T ** 8 + E / T ** 9
 	return second_Virial_Coefficient
-elif property_thermodynamics == "Liquid_Viscosity":
-	liquid_Viscosity = np.exp(A + B / Temp_vector + C * np.log(Temp_vector) + D * Temp_vector ** E)
+
+def liquid_Viscosity(T):
+	liquid_Viscosity = np.exp(A + B / T + C * np.log(T) + D * T ** E)
 	return liquid_Viscosity
-elif property_thermodynamics == "Vapour_Viscosity":
-	vapour_Viscosity = A*Temp_vector**B/(1+C/Temp_vector+D/Temp_vector**2)
+
+def vapour_Viscosity(T):
+	vapour_Viscosity = A * T ** B / (1 + C / T + D / T ** 2)
 	return vapour_Viscosity
-elif property_thermodynamics == "Liquid_Thermal_Conductivity":
-	liquid_Thermal_Conductivity = A+B*Temp_vector+C*Temp_vector**2+D*Temp_vector**3+E*Temp_vector**4
+
+def liquid_Thermal_Conductivity(T):
+	liquid_Thermal_Conductivity = A + B * T + C * T ** 2 + D * T ** 3 + E * T ** 4
 	return liquid_Thermal_Conductivity
-elif property_thermodynamics == "Vapour_Thermal_Conductivity":
-	vapour_Thermal_Conductivity = A * Temp_vector ** B / (1 + C / Temp_vector + D / Temp_vector ** 2)
+
+def vapour_Thermal_Conductivity(T):
+	vapour_Thermal_Conductivity = A * T ** B / (1 + C / T + D / T ** 2)
 	return vapour_Thermal_Conductivity
-elif property_thermodynamics == "Surface_Tension":
+
+def surface_Tension(T, Tc):
+	Tr = T / Tc
 	surface_Tension = A * (1 - Tr) ** (B + C * Tr + D * Tr * 2)
 	return surface_Tension
+
+
+if property_thermodynamics == "Solid_Density":
+	return solid_Density(T)
+elif property_thermodynamics == "Liquid_Density":
+	return liquid_Density(T)
+elif property_thermodynamics == "Vapour_Pressure":
+	return vapour_Pressure(T)
+elif property_thermodynamics == "Heat_of_Vaporization":
+	return heat_of_Vaporization(T, Tc)
+elif property_thermodynamics == "Solid_Heat_Capacity":
+	return solid_Heat_Capacity(T)
+elif property_thermodynamics == "Liquid_Heat_Capacity":
+	return liquid_Heat_Capacity(T, Tc)
+elif property_thermodynamics == "Ideal_Gas_Heat_Capacity":
+	return ideal_Gas_Heat_Capacity(T)
+elif property_thermodynamics == "Second_Virial_Coefficient":
+	return second_Virial_Coefficient(T)
+elif property_thermodynamics == "Liquid_Viscosity":
+	return liquid_Viscosity(T)
+elif property_thermodynamics == "Vapour_Viscosity":
+	return vapour_Viscosity(T)
+elif property_thermodynamics == "Liquid_Thermal_Conductivity":
+	return liquid_Thermal_Conductivity(T)
+elif property_thermodynamics == "Vapour_Thermal_Conductivity":
+	return vapour_Thermal_Conductivity(T)
+elif property_thermodynamics == "Surface_Tension":
+	return surface_Tension(T, Tc)
+
+
+if Solid_Density:
+	return solid_Density(T)
+elif Liquid_Density:
+	return liquid_Density(T)
+elif Vapour_Pressure:
+	return vapour_Pressure(T)
+elif Heat_of_Vaporization:
+	return heat_of_Vaporization(T, Tc)
+elif Solid_Heat_Capacity:
+	return solid_Heat_Capacity(T)
+elif Liquid_Heat_Capacity:
+	return liquid_Heat_Capacity(T, Tc)
+elif Ideal_Gas_Heat_Capacity:
+	return ideal_Gas_Heat_Capacity(T)
+elif Second_Virial_Coefficient:
+	return second_Virial_Coefficient(T)
+elif Liquid_Viscosity:
+	return liquid_Viscosity(T)
+elif Vapour_Viscosity:
+	return vapour_Viscosity(T)
+elif Liquid_Thermal_Conductivity:
+	return liquid_Thermal_Conductivity(T)
+elif Vapour_Thermal_Conductivity:
+	return vapour_Thermal_Conductivity(T)
+elif Surface_Tension:
+	return surface_Tension(T, Tc)
+else:
+	print("No hay propiedad termodinámica")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
